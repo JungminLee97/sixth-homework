df953d5ff5ccb13c88a43a2ac1ceea94d5b6c07a
JungminLee97 <jungmin7618@naver.com>
2025-02-05 23:54:09 +0900

    숙제 끄읕ㅠ

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,11 @@
+import RootLayout from "./layout/RootLayout";
 import TodoContainer from "./TodoList/TodoContainer";
 const TodoListInit = () => {
-  return <TodoContainer />;
+  return (
+    <RootLayout>
+      <TodoContainer />;
+    </RootLayout>
+  );
 };
 
 export default TodoListInit;
--- src/TodoList/TodoContainer.jsx
+++ src/TodoList/TodoContainer.jsx
@@ -1,6 +1,7 @@
 import { useState } from "react";
 import TodoList from "./TodoList";
 import TodoForm from "./TodoForm";
+import TodoDashboard from "./TodoDashboard";
 const SAMPLE_TODOS = [
   { id: 1, text: "Buy milk", completed: false },
   { id: 2, text: "Clean the house", completed: false },
@@ -37,6 +38,7 @@ const TodoContainer = () => {
 
   return (
     <div>
+      <TodoDashboard />
       <TodoForm addTodos={addTodos} />
 
       <TodoList
